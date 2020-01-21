8b56a60bfa1e232aea734f7b1b541fc15e71b48d
Jhosep Davila <jhosepdb149@gmail.com>
2020-01-20 22:28:13 -0300

    add image with link source code

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,39 +1,48 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-import Button from '@material-ui/core/Button'
-import { useTranslations } from '../translations'
+import Button from "@material-ui/core/Button";
+import gitHub from "./img/github.png";
 
 const Root = styled.footer`
-  margin-top:2%;
+  margin-top: 2%;
   background: #fff;
   font-size: 1.1em;
   padding: 1.5625rem;
   text-align: center;
   box-shadow: 0px 0px 70px rgba(102, 102, 102, 0.2);
-  display:flex;
-  justify-content:flex-end;
-  & > *{
-    margin:2%;
+  display: flex;
+  justify-content: flex-end;
+  & > p {
+    margin: 2%;
   }
-`
+`;
 const CustomButton = styled(Button)`
-  height:50px;
-  color:black;
-`
-
+  height: 50px;
+  color: black;
+`;
+const Div = styled.div`
+  width: 50px;
+  height: 50px;
+`;
+const Img = styled.img`
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+`;
 const Footer = () => {
-  const { translate: t } = useTranslations()
-  return(
-  <Root>
-    <p>Copyright © 2020 Jhosep Davila Bustamante</p>
-    <CustomButton 
-    variant="outlined" 
-    color="secondary"
-    href="https://github.com/jhosep98/PorfolioJdb/tree/master"
-    target="blank"
-    >{t('footer.button')}</CustomButton>
-  </Root>
-)
-}
-export default Footer
+  return (
+    <Root>
+      <p>Copyright © 2020 Jhosep Davila Bustamante</p>
+      <Div>
+        <a
+          href="https://github.com/jhosep98/PorfolioJdb/tree/master"
+          target="blank"
+        >
+          <Img src={gitHub} />
+        </a>
+      </Div>
+    </Root>
+  );
+};
+export default Footer;
--- webpack.config.js
+++ webpack.config.js
@@ -4,44 +4,44 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env, argv) => {
   return {
-  entry: path.resolve(__dirname, "src/index.js"),
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Jhosep Davila",
-      template: "src/index.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /^node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: 'file-loader'
-      },
-      {
-        test: /\.(yaml|yml)$/,
-        use: ['json-loader', 'yaml-loader']
-      },
-    ]
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: argv.mode === 'production' ? 'https://jhosep98.github.io/PorfolioJdb/' : '/',
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, "dist")
+    entry: path.resolve(__dirname, "src/index.js"),
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Jhosep Davila",
+        template: "src/index.html"
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /^node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/,
+          use: 'file-loader'
+        },
+        {
+          test: /\.(yaml|yml)$/,
+          use: ['json-loader', 'yaml-loader']
+        },
+      ]
+    },
+    resolve: {
+      extensions: ["*", ".js", ".jsx"]
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      publicPath: argv.mode === 'production' ? 'https://jhosep98.github.io/PorfolioJdb/' : '/',
+      filename: "bundle.js"
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, "dist")
+    }
   }
-}
 }
